b77d5d8fa3064fa76ca134a38f225a3c4e25ff5b
daimaho <135735116+daimaho@users.noreply.github.com>
2025-05-10 17:43:41 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,16 +6,50 @@ app.use(express.json());
 app.post('/render', async (req, res) => {
   const { titulo, imagen } = req.body;
 
-  const html = \`<!DOCTYPE html><html><head><meta charset="UTF-8"><style>
-    body { margin:0;width:1080px;height:1350px;background:url('https://i.imgur.com/LAoeaiT.png') bottom center/cover no-repeat;
-      font-family:'Arial',sans-serif;display:flex;align-items:flex-end;justify-content:center;position:relative;overflow:hidden;}
-    .destacada { position:absolute;top:0;left:0;width:1080px;height:960px;object-fit:cover; }
-    .titulo { position:absolute;bottom:100px;font-size:58px;text-align:center;color:white;
-      text-shadow:2px 2px 4px black;width:100%;padding:0 40px; }
-    </style></head><body>
-    <img class="destacada" src="\${imagen}">
-    <div class="titulo">\${titulo}</div>
-  </body></html>\`;
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+      <style>
+        body {
+          margin: 0;
+          width: 1080px;
+          height: 1350px;
+          background: url('https://i.imgur.com/LAoeaiT.png') bottom center / cover no-repeat;
+          font-family: 'Arial', sans-serif;
+          display: flex;
+          align-items: flex-end;
+          justify-content: center;
+          position: relative;
+          overflow: hidden;
+        }
+        .destacada {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 1080px;
+          height: 960px;
+          object-fit: cover;
+        }
+        .titulo {
+          position: absolute;
+          bottom: 100px;
+          font-size: 58px;
+          text-align: center;
+          color: white;
+          text-shadow: 2px 2px 4px black;
+          width: 100%;
+          padding: 0 40px;
+        }
+      </style>
+    </head>
+    <body>
+      <img class="destacada" src="${imagen}">
+      <div class="titulo">${titulo}</div>
+    </body>
+    </html>
+  `;
 
   const browser = await puppeteer.launch({
     headless: 'new',
